515707949c20d56c80a5b43b1514fb0941dd3d4b
demirhany <demirhanyilal0@gmail.com>
2025-05-15 17:32:28 +0300

    feat: user create dto and user repository are done

---
+++ spring_security_jwt/src/main/java/org/ozo/spring_security_jwt/dto/UserCreateRequest.java
@@ -0,0 +1,13 @@
+package org.ozo.spring_security_jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCreateRequest {
+    private String username;
+    private String password;
+}
